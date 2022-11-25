5ae261d1ac088953053ef553e1ea80d3b7cd4105
Vladislav <sharipovvf@gmail.com>
2022-11-26 02:38:27 +0300

    Выполнил первую часть с количеством человек.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,23 @@
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int friendsNumber = 1;
+        int friendsNumber;
         System.out.println("На скольких человек необходимо разделить счёт?");
         
         Scanner scanner = new Scanner(System.in);
             
-        while (friendsNumber <= 1){
+        while (true){
             friendsNumber = scanner.nextInt();
-            System.out.println("Упс! Для верного расчёта количество челове должно быть больше 1. Попробуйте ещё раз:");
+            if (friendsNumber > 1) {
+                break;
+            }
+            System.out.println("Упс! Для верного расчёта количество человек должно быть больше 1. Попробуйте ещё раз:");
         }
+        System.out.println("Введите название товара");
+        String name = scanner.nextLine();
+        System.out.println(name);
     }
 }
     
 
-public class Goods {
-    String goodName;
-    float goodPrice;
-}
